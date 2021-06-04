e9cb5a8300acd0de8666224e2fb4b761f96fe702
Tam Ngoc Bang Nguyen <tam.ngocbang.nguyen@gmail.com>
2021-06-04 22:39:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myrunningapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myrunningapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
