fcccdb15214175d6ffc0e01b9cfe344f16e6e6ad
keepgoing0901 <57678065+keepgoing0901@users.noreply.github.com>
2021-06-06 12:55:56 +0700

    new commit

--- app/src/main/java/com/example/myrunningapp/MainActivity.java
+++ app/src/main/java/com/example/myrunningapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myrunningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton reminderButton = findViewById(R.id.reminder);
+        reminderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(MainActivity.this);
+
+            }
+        });
     }
 }
