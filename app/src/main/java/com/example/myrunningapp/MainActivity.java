3943866f9f6eaafa8dcb4aa9a329eb2940ee55d6
Tam Ngoc Bang Nguyen <tam.ngocbang.nguyen@gmail.com>
2021-06-06 22:42:41 +0700

    First intent: Alarm Clock

--- app/src/main/java/com/example/myrunningapp/MainActivity.java
+++ app/src/main/java/com/example/myrunningapp/MainActivity.java
@@ -3,10 +3,20 @@ package com.example.myrunningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +28,40 @@ public class MainActivity extends AppCompatActivity {
         reminderButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Dialog dialog = new Dialog(MainActivity.this);
+                final Dialog dialog = new Dialog(MainActivity.this, android.R.style.Theme_Material_Dialog_NoActionBar);
+                dialog.setContentView(R.layout.reminder_dialog);
+                dialog.setCancelable(false);
 
+                final EditText message = dialog.findViewById(R.id.message);
+                final TimePicker timePicker = dialog.findViewById(R.id.time_picker);
+                Button cancelButton = dialog.findViewById(R.id.cancel_button);
+                Button okButton = dialog.findViewById(R.id.ok_button);
+                cancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                okButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int minute = timePicker.getMinute();
+                        int hour = timePicker.getHour();
+                        String myMessage = message.getText().toString();
+                        if (myMessage.trim().isEmpty())
+                            myMessage = "My Workout";
+
+                        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                                .putExtra(AlarmClock.EXTRA_MESSAGE, myMessage)
+                                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                                .putExtra(AlarmClock.EXTRA_MINUTES, minute);
+                        if (intent.resolveActivity(getPackageManager()) != null) {
+                            startActivity(intent);
+                        }
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
             }
         });
     }
